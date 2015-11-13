101671bde742eaf5f667a901bdfd55fea28ecb8a
Hiro Yamada <yak123soba@gmail.com>
2015-11-13 14:14:25 +0100

    about to allow eof

--- test_compress.sh
+++ test_compress.sh
@@ -1,15 +1,17 @@
 dir=../out/production/INF558_Project/
+rm -rf playground
+mkdir playground
+java -classpath $dir bw.BWTransformer $1 > playground/BW
+java -classpath $dir m2f.M2FCoder playground/BW > playground/MTF
 
-java -classpath $dir BWTransformer donner_unformated.txt > playground/BW
-java -classpath $dir MTFCoder playground/BW > playground/MTF
+compress -fc playground/MTF > playground/BW_M2F_compress
+gzip -fc playground/MTF > playground/BW_M2F_gzip
+bzip2 -fc playground/MTF > playground/BW_M2F_bzip2
+java -classpath $dir adaptive_huffman.HuffmanCoder playground/MTF playground/BW_M2F_huffman
 
-compress -fc playground/MTF > playground/MTF.Z
-gzip -fc playground/MTF > playground/MTF.gzip
-bzip2 -fc playground/MTF > playground/MTF.bzip2
-
-
-compress -fc donner_unformated.txt > playground/compressed_lz
-gzip -fc donner_unformated.txt > playground/compressed_gzip
-bzip2 -fc donner_unformated.txt > playground/compressed_bzip2
-ls -l donner_unformated.txt
+compress -fc donner_unformated.txt > playground/original_compress
+gzip -fc donner_unformated.txt > playground/original_gzip
+bzip2 -fc donner_unformated.txt > playground/original_bzip2
+java -classpath $dir adaptive_huffman.HuffmanCoder donner_unformated.txt playground/original_huffman
+ls -l $1
 ls -l playground
