06b1cc417f8b2ca9d87ac8f5e12f7b73e69d3016
Hiro Yamada <yak123soba@gmail.com>
2015-11-15 14:31:22 +0100

    python code formatted

--- scripts/chart.py
+++ scripts/chart.py
@@ -2,8 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-with open ('results', 'r') as f:
-    for y in range(0,10):
+with open('results', 'r') as f:
+    for y in range(0, 10):
         f.readline()
         line = f.readline()
         total = line.split()[4]
@@ -12,9 +12,9 @@ with open ('results', 'r') as f:
         values = {}
         values["raw"] = int(total)
 
-        for x in range (0,5):
+        for x in range(0, 5):
             line = f.readline().split()
-            values[line[8][line[8].index('_') + 1 :]] = int(line[4])
+            values[line[8][line[8].index('_') + 1:]] = int(line[4])
         plt.bar(range(len(values)), sorted(values.values()), align='center')
         plt.xticks(range(len(values)), sorted(values, key=values.get))
         plt.savefig("image" + str(y))
